cdadf673946780f6e5470abc3b4f4fe1533fd37c
adhoc <adhoc@adhocs-imac.local>
2016-04-17 14:45:21 -0700

    first commit

--- anim/ViewController.swift
+++ anim/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var img: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        self.img.alpha=0
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration(4) {
+            self.img.alpha=1
+        }
+    }
 
 }
 
